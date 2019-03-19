860e899ab1e5b181ed8260d6f39fee0e64a293e1
Joey <hobiger.j@gmail.com>
2019-03-19 20:04:20 +0100

    einlesen-methode von vivienne

--- A3/MTP.py
+++ A3/MTP.py
@@ -6,18 +6,29 @@ matrix1 = []
 
 def read_in_matrix():
     for line in sys.stdin:
-        if line.startswith("G"):
-            continue
+        line_list = line.strip("  ").strip(" \n").split("   ")
+        #print(line_list)
 
-        if line.startswith("---"):
-            break
-        print(line)
-        
+
+
+    return matrix1, matrix2
 
 
 print(read_in_matrix())
 
 
-uebungs_matrix = [[1,2,3], 
-                  [4,5,6], 
-                  [7,8,9]]
+"""
+G_down: 4 5
+  0.12   0.79   0.50   0.56   0.39
+  0.93   0.14   0.82   0.80   0.13
+  0.71   0.37   0.49   0.94   0.88
+  0.59   0.52   0.40   0.87   0.16
+---
+G_right: 5 4
+  0.43   0.21   0.55   0.61
+  0.61   0.89   0.52   0.54
+  0.44   0.85   0.74   0.12
+  0.56   0.91   0.61   0.24
+  0.56   0.42   0.27   0.49
+---
+"""
