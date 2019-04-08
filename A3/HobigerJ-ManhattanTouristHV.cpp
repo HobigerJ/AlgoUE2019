14570d3ef2d5ddcd471206be1d62156238f931af
Joey <hobiger.j@gmail.com>
2019-04-08 14:26:16 +0200

    aufgabe 3, manhT horiz vert

--- A3/HobigerJ-ManhattanTouristHV.cpp
+++ A3/HobigerJ-ManhattanTouristHV.cpp
@@ -9,9 +9,7 @@
 
 using namespace std;
 
-string read_in(string filename);
-string Angabe = read_in("rmHV_10_12");
-
+//Klasse:
 class Matrix {
     private:
         int werteanz;
@@ -70,18 +68,47 @@ class Matrix {
             
             spaltenanz = number_values_matrix1 / number_rows_matrix1;       
             zeilenanz = number_rows_matrix1;
-            //matrix_values = new float[zeilenanz * spaltenanz];
             setDimensions();  
         }
 
         void dimensions_matrix_right(Matrix m) {
             spaltenanz = m.getSpaltenanz() - 1;
-            zeilenanz = m.getZeilenanz() + 1;
-            //matrix_values = new float[zeilenanz * spaltenanz];  
+            zeilenanz = m.getZeilenanz() + 1;  
             setDimensions();  
         }
 
 };
+
+//globale Variable:
+int cursor = 0; 
+
+// Prototypen:
+string read_in(string filename);
+int fill_matrix(Matrix m, int &cursor, string Angabe);
+void calculate_node_sums(Matrix nodes, Matrix down, Matrix right);
+
+// Main:
+int main(int argc, char* argv[]) { 
+
+    string Angabe = read_in(argv[1]);
+
+    Matrix down_edges;
+    down_edges.dimensions_matrix_down(Angabe);   
+
+    Matrix right_edges;
+    right_edges.dimensions_matrix_right(down_edges);
+
+    fill_matrix(down_edges, cursor, Angabe);
+    
+    fill_matrix(right_edges, cursor, Angabe);
+
+    Matrix knoten;
+    knoten.setSpaltenanz(down_edges.getSpaltenanz());
+    knoten.setZeilenanz(right_edges.getZeilenanz());
+    knoten.setDimensions();
+
+    calculate_node_sums(knoten, down_edges, right_edges);
+}
 
 string read_in(string filename) {
         ifstream input_file (filename); 
@@ -94,8 +121,6 @@ string read_in(string filename) {
        
 }
 
-int cursor = 0;
-  
 int fill_matrix(Matrix m, int &cursor, string Angabe) {
     for (int y = 0; y < m.getZeilenanz(); y++) {
         for (int x = 0; x < m.getSpaltenanz(); x++) {
@@ -121,75 +146,39 @@ int fill_matrix(Matrix m, int &cursor, string Angabe) {
 
 } 
 
-int main() { 
-
-    Matrix down_edges;
-    down_edges.dimensions_matrix_down(read_in("rmHV_10_12"));   
-    //cout << "Matrix 1" << endl;
-    //cout << "Spaltenanz: " << down_edges.getSpaltenanz() << " und Reihenanz: " << down_edges.getZeilenanz() << endl;
-
-    Matrix right_edges;
-    right_edges.dimensions_matrix_right(down_edges);
-    //cout << "Matrix 2" << endl;
-    //cout << "Spalten: " << right_edges.getSpaltenanz() << " und Reihen: " << right_edges.getZeilenanz() << endl;
-
-    fill_matrix(down_edges, cursor, Angabe);
-    //cout << down_edges.getMatrixValue(9,8) << endl; // letzt Koordinate bei 9,8
-    //cout << "cursor: " << cursor << endl; // cursor am schluss bei 648
-
-    fill_matrix(right_edges, cursor, Angabe);
-    //cout << right_edges.getMatrixValue(7,7) << endl; // letzte Koordinate bei 
-    //cout << "cursor: " << cursor << endl; // cursor am schluss bei 1306
-
-    Matrix knoten;
-    knoten.setSpaltenanz(down_edges.getSpaltenanz());
-    knoten.setZeilenanz(right_edges.getZeilenanz());
-    knoten.setDimensions();
-    //cout << " spalten: " << knoten.getSpaltenanz() << " und Zeilen: " << knoten.getZeilenanz() << endl;
-
-
+void calculate_node_sums(Matrix nodes, Matrix down, Matrix right) {
     float value = 0.0;    
     
-    for (int y = 0; y < knoten.getZeilenanz(); y++) {
+    for (int y = 0; y < nodes.getZeilenanz(); y++) {
         if (y == 0){
-            knoten.setValue(0,0, 0.0);
+            nodes.setValue(0,0, 0.0);
         } else {
-            value = knoten.getValue(0,y-1) + down_edges.getValue(0,y-1);
-            knoten.setValue(0,y, value);
-            cout << knoten.getValue(0,y) << ", " << endl;
+            value = nodes.getValue(0,y-1) + down.getValue(0,y-1);
+            nodes.setValue(0,y, value);
             }
     }
 
-    for (int x = 0; x < knoten.getSpaltenanz(); x++) {
+    for (int x = 0; x < nodes.getSpaltenanz(); x++) {
         if (x == 0){
-            knoten.setValue(0,0, 0.0);
+            nodes.setValue(0,0, 0.0);
         } else {
-            value = knoten.getValue(x-1,0) + right_edges.getValue(x-1,0);
-            knoten.setValue(x,0, value);
-            cout << knoten.getValue(x,0) << ", " << endl;
+            value = nodes.getValue(x-1,0) + right.getValue(x-1,0);
+            nodes.setValue(x,0, value);
             }
     }
 
     float value1 = 0;
     float value2 = 0;
     float max_value = 0;
-    for (int y = 1; y < knoten.getSpaltenanz(); y++) {
-        for (int x = 1; x < knoten.getZeilenanz(); x++) {
-            value1 = knoten.getValue(x,y-1) + down_edges.getValue(x, y-1);
-            value2 = knoten.getValue(x-1,y) + right_edges.getValue(x-1,y);
-            cout << "wert1: " << value1 << ", wert2: " << value2 << endl;
+    for (int y = 1; y < nodes.getSpaltenanz(); y++) {
+        for (int x = 1; x < nodes.getZeilenanz(); x++) {
+            value1 = nodes.getValue(x,y-1) + down.getValue(x, y-1);
+            value2 = nodes.getValue(x-1,y) + right.getValue(x-1,y);
             max_value = max(value1, value2);
-            knoten.setValue(x,y, max_value);
+            nodes.setValue(x,y, max_value);
         }
     }
-
-    cout << "maximale kantenlaenge: " << knoten.getValue(knoten.getSpaltenanz()-1, knoten.getZeilenanz()-1) << endl; // 5.97
-
+    cout << "weight of the longest path: " << nodes.getValue(nodes.getSpaltenanz()-1, nodes.getZeilenanz()-1) << endl;
 }
-
-
-    
-
-
 
 
